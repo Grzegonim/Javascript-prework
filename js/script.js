f3b99af3cd8919467c53df2808b7208738aee18e
Grzegorz Woźniak <grzegorz.wozniak1989@gmail.com>
2022-06-04 15:53:15 +0200

    Add computerMove

--- js/script.js
+++ js/script.js
@@ -1,3 +1,15 @@
-let playerMove = 'papier';
-let computerMove = `kamień`;
-printMessage('Zagrałem ' + playerMove + '! Jeśli Twój ruch to nożyczki, to wygrywasz!');
+let randomNumber = Math.floor(Math.random() * 3 + 1);
+
+console.log('Wylosowana liczba to: ' + randomNumber);
+
+let computerMove = 'nieznany ruch';
+
+if(randomNumber == 1){
+  computerMove = 'kamień';
+} if(randomNumber == 2){
+  computerMove = 'papier';
+} if(randomNumber == 3){
+  computerMove = 'nożyce';
+}
+
+printMessage('Mój ruch to: ' + computerMove);
